4d77f7232b091ce2e977b7636aabd3681934db42
danielcr0995 <daniel.correa.1995@gmail.com>
2021-12-20 23:09:05 -0500

    refactoring js file

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ var imagedice2=document.querySelector(".img2");
 var title=document.querySelector("h1")
 
 if(randomNumber1>randomNumber2){
-    title.innerText="Player 1 Wins!";
+    title.innerText="🚩 Player 1 Wins!";
 }else if(randomNumber1<randomNumber2){
-    title.innerText="Player 2 Wins!";
+    title.innerText="Player 2 Wins! 🚩";
 }else{
     title.innerText="Draw!";
 }
 
-var imageName1=newimgName(randomNumber1);
-var imageName2=newimgName(randomNumber2);
+var imageName1= "images/dice"+randomNumber1+".png"
+var imageName2= "images/dice"+randomNumber2+".png"
+// var imageName1=newimgName(randomNumber1);
+// var imageName2=newimgName(randomNumber2);
 // if (randomNumber1===1){
 //     imageName="dice1.png";
 // }else if (randomNumber1===2){
@@ -32,35 +34,29 @@ var imageName2=newimgName(randomNumber2);
 //     imageName="dice6.png";
 // }
 
-var newimg1="./images/"+imageName1;
-var newimg2="./images/"+imageName2;
+// var newimg1="./images/"+imageName1;
+// var newimg2="./images/"+imageName2;
 
-var newDice1=imagedice1.setAttribute("src", newimg1);
-var newDice1=imagedice2.setAttribute("src", newimg2);
-
-if(randomNumber1>randomNumber2){
-    newTitle="Player 1 Wins!";
-}else if(randomNumber1<randomNumber2){
-    newTitle="Player 2 Wins!";
-}else{
-    newTitle="Draw!";
-}
+var newDice1=imagedice1.setAttribute("src", imageName1);
+var newDice1=imagedice2.setAttribute("src", imageName2);
 
 
 
-function newimgName(randomNumber){
-    if (randomNumber===1){
-        imageName="dice1.png";
-    }else if (randomNumber===2){
-        imageName="dice2.png";
-    }else if (randomNumber===3){
-        imageName="dice3.png";
-    }else if (randomNumber===4){
-        imageName="dice4.png";
-    }else if (randomNumber===5){
-        imageName="dice5.png";
-    }else{
-        imageName="dice6.png";
-    }
-    return imageName;
-}
+
+
+// function newimgName(randomNumber){
+//     if (randomNumber===1){
+//         imageName="dice1.png";
+//     }else if (randomNumber===2){
+//         imageName="dice2.png";
+//     }else if (randomNumber===3){
+//         imageName="dice3.png";
+//     }else if (randomNumber===4){
+//         imageName="dice4.png";
+//     }else if (randomNumber===5){
+//         imageName="dice5.png";
+//     }else{
+//         imageName="dice6.png";
+//     }
+//     return imageName;
+// }
